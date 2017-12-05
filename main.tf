030b9203acb462f9ef2c66e60d7fd83351dcf9e1
root <root@kubernetes1.c.secure-grammar-97910.internal>
2017-12-05 03:05:40 +0000

    Security group update

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "testserver" {
 
 ami = "ami-55ef662f"
 instance_type = "t2.micro"
+vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              echo "hello world" > /var/www/html/index.html
+              service httpd start
+              EOF
+
 
 tags
 {
@@ -13,3 +21,27 @@ Name = "mytestserver"
 }
 
 }
+
+resource "aws_security_group" "instance" {
+
+name = "example-sg"
+
+ingress {
+
+from_port = 80
+to_port  = 80
+protocol  = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+egress {
+from_port = 80
+to_port  = 80
+protocol  = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+
+
